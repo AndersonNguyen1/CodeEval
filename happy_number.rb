7df77b0841068eb779268dad0e57560039571ca2
Anderson Nguyen <anguyen135@gmail.com>
2016-06-30 12:13:56 -0700

    Modified Code WIP

--- happy_number.rb
+++ happy_number.rb
@@ -1,10 +1,23 @@
+def happy_check(number_string)
+
+end
+
+
+number_list = []
+solutions = []
+
+
+
 File.open(ARGV[0]).each_line do |line|
-  splitNumbers = line.strip.split('')
-  if splitNumbers == 1
-    puts('true')
-  # else
-  #   splitNumbers != 1
-  #   splitNumbers.each_char do
+  splitNumbers = line.strip.split ''
+
+  splitNumbers.each_index do |index|
+    if splitNumbers[index].to_i == 1
+      solutions << 1
+    else
+      happy_check(line)
+    end
   end
 end
-#print a.join('')
+
+puts solutions
