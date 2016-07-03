e4b360652d4dc0ab36aa65563cb401fe379251ca
Anderson Nguyen <anguyen135@gmail.com>
2016-07-03 14:08:09 -0700

    More CodeEval

---
+++ find_the_highest_score.rb
@@ -0,0 +1,10 @@
+File.open(ARGV[0]).each_line do |line|
+  highest = []
+  table = [][]
+
+  a = line.strip.split('| ').each do |row|
+    row.split ''
+  puts a
+  puts b
+  puts c
+end
--- happy_number.rb
+++ happy_number.rb
@@ -1,20 +1,38 @@
-def happy_check(number_string)
+# numbers_list = []
+# solutions = []
+#
+# def happy_check(number_string, numbers_list, solutions)
+#   sum = 0
+#   if number_string.to_i == 1
+#     solutions << 1
+#   else
+#     split_numbers = number_string.strip.split ''
+#     split_numbers.each_index do |index|
+#       sum += split_numbers[index].to_i**2
+#     end
+#     numbers_list << sum
+#     puts solutions
+#   end
+# end
+#
+# File.open(ARGV[0]).each_line do |line|
+#   happy_check(line, numbers_list, solutions)
+# end
+
+def happy_check(x)
+  return 1 if number.to_i == 1
+  # check if the number is one
+  # otherwise, split the number and square each number
+  # sum them back together and store it into an array
+  # make
+
+def happy?(number)
+  history = {}
+  happy_check(history)
+  return 0
 
 end
-
-number_list = []
-solutions = []
 
 File.open(ARGV[0]).each_line do |line|
-  splitNumbers = line.strip.split ''
-
-  splitNumbers.each_index do |index|
-    if splitNumbers[index].to_i == 1
-      solutions << 1
-    else
-      happy_check(splitNumbers)
-    end
-  end
+  puts happy?(line)
 end
-
-puts solutions
