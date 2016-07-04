dcff4e92aa2ae28aea8c0299fb33748af5995c96
Anderson Nguyen <anguyen135@gmail.com>
2016-07-04 11:09:52 -0700

    Modified

--- find_the_highest_score.rb
+++ find_the_highest_score.rb
@@ -1,10 +1,31 @@
-File.open(ARGV[0]).each_line do |line|
-  highest = []
-  table = [][]
+first = []
+second = []
+third = []
 
-  a = line.strip.split('| ').each do |row|
-    row.split ''
-  puts a
-  puts b
-  puts c
+File.open(ARGV[0]).each_line do |line|
+  a, b, c = line.strip.split('| ')
+
+  a = a.split ' '
+  a = a.map(&:to_i)
+  b = b.split ' '
+  b = b.map(&:to_i)
+  c = c.split ' '
+  c = c.map(&:to_i)
+
+  first << a[0]
+  second << a[1]
+  third << a[2]
+
+  first << b[0]
+  second << b[1]
+  third << b[2]
+
+  first << c[0]
+  second << c[1]
+  third << c[2]
+
+  first.sort!
+  second.sort!
+  third.sort!
+  puts "#{first.last} #{second.last} #{third.last}"
 end
