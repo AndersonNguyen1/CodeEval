ba41a1b6702ba3e4fc0fe2e6d5ecb3dd1bd7c32e
Anderson Nguyen <anguyen135@gmail.com>
2016-07-09 17:46:10 -0700

    More CodeEval

--- compressed_sequence.rb
+++ compressed_sequence.rb
@@ -1,14 +1,23 @@
+
+
 File.open(ARGV[0]).each_line do |line|
-  a = line.strip.split
+  a = line.strip.split ' '
+
+  final_array = []
+  
   count = 1
-  final = []
+  current_number = nil
+  final_string = nil
+
   a.each_index do |index|
     if a[index] == a[index + 1]
       count += 1
+    else
+      current_number = a[index]
+      final_array << count
+      final_array << current_number
+      count = 1
     end
-    #final << count
-    #final << a[index]
   end
-  print count
-  #print final
+  puts final_array.join ' '
 end
