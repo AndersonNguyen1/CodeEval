454b7bdc7b485662b208a9e7cdfc9538e8c0f71e
Anderson Nguyen <anguyen135@gmail.com>
2016-07-10 21:37:35 -0700

    More CodeEval

--- roller_coaster.rb
+++ roller_coaster.rb
@@ -1,2 +1,16 @@
 File.open(ARGV[0]).each_line do |line|
-  line.
+  characters = line.strip.split ''
+  upper_toggle = true
+  characters.each_index do |index|
+    if characters[index] =~ /[a-zA-Z]+/
+      if upper_toggle
+        characters[index].upcase!
+        upper_toggle = false
+      else
+        characters[index].downcase!
+        upper_toggle = true
+      end
+    end
+  end
+  puts characters.join ''
+end
